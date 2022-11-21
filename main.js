91f8f06f19c44f73ec3c772c0192b0613423f1a3
miloskolarov <milos.v.kolarov@gmail.com>
2022-11-21 11:56:04 +0100

    Start working on functionality

---
+++ main.js
@@ -0,0 +1,13 @@
+let currentNumb= "";
+let prevNumb="";
+let operator= "";
+
+const currentDisplayNum= document.querySelector ("");
+const previousDisplayNum= document.querySelector ("");
+
+const equal= document.querySelector ("");
+const numb= document.querySelector ("");
+const operators= document.querySelector ("");
+const numbers= document.querySelecor ("");
+const clear= document.querySelector ("");
+
